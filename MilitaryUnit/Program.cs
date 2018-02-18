82b38aea752dc4adffaf71888326013c8a0e3ed9
Roy Lovitt <lovittl@my.erau.edu>
2018-02-18 14:58:24 -0500

    create splash page
    Closes #1

--- MilitaryUnit/Program.cs
+++ MilitaryUnit/Program.cs
@@ -10,6 +10,17 @@ namespace MilitaryUnit
     {
         static void Main(string[] args)
         {
+            // Splash screen explaining a background on the game
+            Console.WriteLine("** WELCOME TO Military Unit! **\n");
+            Console.WriteLine("Go on Missions, Deploy Personell, Move Vehicles, Shoot Weapons. \n");
+            Console.WriteLine("This is a Classroom Exercise to demonstrate understanding" +
+                " of Inheritance.\n");
+                        Console.WriteLine("Hit enter to continue.");
+            Console.ReadLine();
+
+            //Creates an instance of the MilitaryUnit class
+            //MilitaryUnit MUnit = new MilitaryUnit();
+            //MUnit.Run();
         }
     }
 }
